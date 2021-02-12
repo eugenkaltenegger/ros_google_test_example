3bc98c402b07687e3ae22413fc4a518c0450b404
Eugen <eugen.kaltenegger@gmail.com>
2021-02-12 13:00:31 +0100

    added accidentally removed part

--- google_test_example/include/non_virtual_talker.hpp
+++ google_test_example/include/non_virtual_talker.hpp
@@ -4,6 +4,26 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 
+namespace NonVirtual { ;
 
+    template<typename T>
+    class Talker {
+
+        private:
+            T *publisher_pointer;
+
+        public:
+
+            void setPublisher(T *publisher) {
+                this->publisher_pointer = publisher;
+            };
+
+            virtual void publish(const std_msgs::String &message) const {
+                ROS_DEBUG("non-virtual publish function is invoked");
+                this->publisher_pointer->publish(message);
+            };
+
+    };
+}
 
 #endif // NON_VIRTUAL_TALKER_H
